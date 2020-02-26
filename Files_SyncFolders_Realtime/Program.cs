00713d3e57053f17571adc53a7ac7d4ca5a9f0ee
Sizel <Meresh976@yandex.ru>
2020-02-26 23:17:02 +0200

    finished

--- Files_SyncFolders_Realtime/Program.cs
+++ Files_SyncFolders_Realtime/Program.cs
@@ -13,116 +13,96 @@ namespace Files_SyncFolders_Realtime
 	{
 		static void Main(string[] args)
 		{
-			string rootPath = @"..\..\..\Dirs";
-			Directory.SetCurrentDirectory(rootPath);
+			string currentDir = @"..\..\..\Dirs";
+			Directory.SetCurrentDirectory(currentDir);
 
-			var startingDir1 = new DirectoryInfo(@".\dir1");
-			var startingDir2 = new DirectoryInfo(@".\dir2");
+			FileSystemWatcher fileWatcher = new FileSystemWatcher(@".\dir1");
 
-			MakeDirsIdentical(startingDir1, startingDir2);
+			fileWatcher.IncludeSubdirectories = true;
+
+			fileWatcher.NotifyFilter = NotifyFilters.Size | NotifyFilters.FileName;
+
+			fileWatcher.Changed += OnFileChange;
+			fileWatcher.Created += OnFileCreate;
+			fileWatcher.Deleted += OnFileDelete;
+			fileWatcher.Renamed += OnFileRename;
+
+			fileWatcher.EnableRaisingEvents = true;
+
+
+			FileSystemWatcher dirWatcher = new FileSystemWatcher(@".\dir1");
+
+			dirWatcher.IncludeSubdirectories = true;
+
+			dirWatcher.NotifyFilter = NotifyFilters.DirectoryName;
+
+			dirWatcher.Created += OnDirCreate;
+			dirWatcher.Deleted += OnDirDelete;
+			dirWatcher.Renamed += OnDirRename;
+
+			dirWatcher.EnableRaisingEvents = true;
+
+			while (Console.Read() != 'q') { }
 		}
 
-		static void MakeDirsIdentical(DirectoryInfo dir1, DirectoryInfo dir2)
+		private static void OnDirRename(object sender, RenamedEventArgs e)
+		{
+			Console.WriteLine($"The name of { e.OldFullPath } was changed to { e.Name } in dir1 and dir2");
+
+			string pathInDir2 = e.OldFullPath.Replace("dir1", "dir2");
+
+			Directory.Move(pathInDir2, e.FullPath.Replace("dir1", "dir2"));
+		}
+
+		private static void OnDirDelete(object sender, FileSystemEventArgs e)
+		{
+			Console.WriteLine($@"The directory { e.FullPath } was deleted in both dir1 and dir2");
+
+			Directory.Delete(e.FullPath.Replace("dir1", "dir2"));
+		}
+
+		private static void OnDirCreate(object sender, FileSystemEventArgs e)
+		{
+			Console.WriteLine($"{ e.FullPath } was created in dir1 and dir2");
+
+			Directory.CreateDirectory(e.FullPath.Replace("dir1", "dir2"));
+		}
+
+
+		private static void OnFileRename(object sender, RenamedEventArgs e)
+		{
+			Console.WriteLine($"The name of { e.OldFullPath } was changed to { e.Name } in dir1 and dir2");
+
+			string pathInDir2 = e.OldFullPath.Replace("dir1", "dir2");
+
+			File.Move(pathInDir2, e.FullPath.Replace("dir1", "dir2"), true);
+		}
+
+		private static void OnFileCreate(object sender, FileSystemEventArgs e)
+		{
+			Console.WriteLine($"{ e.FullPath } was created in dir1 and dir2");
+
+			File.Create(e.FullPath.Replace("dir1", "dir2")).Close();
+		}
+
+		private static void OnFileChange(object sender, FileSystemEventArgs e)
 		{
 
-			#region Copy files
+			Console.WriteLine($@"The content of { e.FullPath } was changed in dir1 and dir2");
 
-			var filesDir1 = dir1.GetFiles("*", SearchOption.TopDirectoryOnly);
-			var filesDir2 = dir2.GetFiles("*", SearchOption.TopDirectoryOnly);
+			FileInfo fi = new FileInfo(e.FullPath);
 
-			// Get files from dir1 that don't exist in dir2 and copy them to dir2
-			var filesToCopy = filesDir1.Except(filesDir2, new FileInfoEqualityComparer());
+			fi.CopyTo(e.FullPath.Replace("dir1", "dir2"), true);
 
-			foreach (var file in filesToCopy)
-			{
-				Console.WriteLine($@"Copying { file.Name } from { dir1.FullName } to { dir2.FullName }");
-				Console.WriteLine();
-
-				file.CopyTo(@$"{ dir2.FullName }\{ file.Name }", true);
-			}
-			#endregion
-
-			#region Delete files
-			// Get files from dir2 that don't exist in dir1 and delete them
-			var filesToDelete = filesDir2.Except(filesDir1, new FileInfoEqualityComparer());
-
-			foreach (var file in filesToDelete)
-			{
-				Console.WriteLine($"Deleting { file.FullName }");
-				Console.WriteLine();
-
-				file.Delete();
-			}
-			#endregion
-
-			#region Copy subdirs
-
-			var subdirsDir1 = dir1.GetDirectories();
-			var subdirsDir2 = dir2.GetDirectories();
-
-			// Get subdirectories in dir1 that don't exist in dir2
-			var subdirsToCopy = subdirsDir1.Except(subdirsDir2, new DirectoryInfoEqualityComparer());
-
-			// and copy them to dir2
-			foreach (var subdir in subdirsToCopy)
-			{
-				Console.WriteLine($"Copying { subdir.Name } from { subdir.FullName } to { dir2.FullName }");
-				Console.WriteLine();
-
-				var subdirTarget = dir2.CreateSubdirectory(subdir.Name);
-
-				CopyAll(subdir, subdirTarget);
-			}
-			#endregion
-
-			#region Delete subdirs
-
-			// Get subdirs in dir2 that don't exist in dir1
-			var dirsToDelete = subdirsDir2.Except(subdirsDir1, new DirectoryInfoEqualityComparer());
-
-			// Delete them
-			foreach (var dir in dirsToDelete)
-			{
-				Console.WriteLine($"Deleting { dir.FullName }");
-				Console.WriteLine();
-
-				dir.Delete(true);
-			}
-			#endregion
-
-			#region Run MakeDirsIdentical on the subdirs with the same name
-
-			// Get names of identical subdirs
-			var identicalSubdirsNames = subdirsDir1.Intersect(subdirsDir2, new DirectoryInfoEqualityComparer()).Select(s => s.Name);
-
-			// Run MakeDirsIdentical recursively on each pair of subdirs with the same name
-			foreach (var subdirName in identicalSubdirsNames)
-			{
-				MakeDirsIdentical(new DirectoryInfo(@$"{ dir1.FullName }\{ subdirName }"), new DirectoryInfo(@$"{ dir2.FullName }\{ subdirName }"));
-			}
-			#endregion
 		}
 
-		static void CopyAll(DirectoryInfo source, DirectoryInfo target)
+		private static void OnFileDelete(object sender, FileSystemEventArgs e)
 		{
-			if (!target.Exists)
-			{
-				Directory.CreateDirectory(target.FullName);
-			}
+			Console.WriteLine($@"The file { e.FullPath } was deleted in both dir1 and dir2");
 
-			// Copy each file into the new directory.
-			foreach (FileInfo fi in source.GetFiles())
-			{
-				fi.CopyTo(Path.Combine(target.FullName, fi.Name), true);
-			}
-
-			// Copy each subdirectory using recursion.
-			foreach (DirectoryInfo diSourceSubDir in source.GetDirectories())
-			{
-				DirectoryInfo nextTargetSubDir = target.CreateSubdirectory(diSourceSubDir.Name);
-				CopyAll(diSourceSubDir, nextTargetSubDir);
-			}
+			File.Delete(e.FullPath.Replace("dir1", "dir2"));
 		}
+
 
 	}
 }
